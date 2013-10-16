a3a7fe7873de72679da0f0d36e7c66047846d3d6
adamhammes <adamhammes@centetune.com>
2013-10-15 23:37:51 -0500

    Set up Mechanize Browser

--- WebClient.py
+++ WebClient.py
@@ -2,3 +2,13 @@ __author__ = 'adamhammes'
 
 import mechanize
 import bs4
+
+br = mechanize.Browser()
+
+br.set_handle_equiv(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+
+br.addheaders = [('User-agent', 'Mozilla/6.0 (X11; U; Linux i686; en-US; rv:1.9.0.1')]
+
