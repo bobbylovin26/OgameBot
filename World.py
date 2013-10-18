eadf141af79cb24a7252ee5a3a80f1bd8b3e91c7
adamhammes <adamhammes@centetune.com>
2013-10-18 10:43:41 -0500

    Started work on World class

--- World.py
+++ World.py
@@ -1,2 +1,16 @@
 __author__ = 'adamhammes'
 
+from BeautifulSoup import BeautifulSoup
+
+class World:
+    def __init__(self, url = None, fileName = None):
+        if fileName:
+            f = open(fileName, 'w')
+            self.soup = BeautifulSoup(f)
+        elif url:
+            self.soup = BeautifulSoup(url)
+
+        setData()
+
+    def setData(self):
+        self.resources =
